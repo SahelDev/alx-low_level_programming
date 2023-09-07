9a597cff44b8401f91ac0aa1615753208e8c6c98
baderou <baderou@yahoo.fr>
2023-09-07 18:15:55 +0100

    task4 done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:'Print hello world'
+ *
+ * Return:Always 0(Success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
