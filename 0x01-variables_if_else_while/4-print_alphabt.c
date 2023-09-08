a466202fa71ad1c365155ed309e056f214d5f1f1
baderou <baderou@yahoo.fr>
2023-09-08 23:49:05 +0100

    task4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints out alphabet
+ * Return: 0 if success
+ *
+ *
+ */
+int main(void)
+{
+char a;
+char new_line = '\n';
+for (a = 'a'; a <= 'z'; a++)
+{
+	if ((a != 'q') || (a != 'e'))
+		putchar(a);
+}
+putchar(new_line);
+return (0);
+}
