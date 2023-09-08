d02f3eadcc17ef3a797d8b1998ad6c7b4749590b
baderou <baderou@yahoo.fr>
2023-09-08 21:34:42 +0100

    task2 done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints out alphabet
+ * Return: 0 if success
+ *
+ *
+ */
+
+int main(void)
+{
+char a = 'a';
+char z = 'z';
+for (; a <= z;)
+{	putchar(a);
+	a++;
+}
+return (0);
+}
