54532074aefe25f7a8de4b42382999ace73f5968
baderou <baderou@yahoo.fr>
2023-09-13 10:27:50 +0100

    task 3 done

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,9 +1,31 @@
 #include "main.h"
+#include<stdio.h>
 
+/**
+ * 3-islower -checks for lowercase character
+ * Return: 1 if lowercase and 0 otherwise
+ *
+ *
+ *
+ */
+
+int _islower(int c)
+{
+	int j;
+	for(j=97; j <= 122; j++)
+	{
+		if (c==j)
+			return (1);
+	}
+	return (0);
+	
+}
 
 
 int main (void)
 {
-	print_alphabet();
-	return (0);
+int character =	_islower('C');
+putchar(character + '0');
+
 }
+
