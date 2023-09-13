df8b16d87d1b6ec7d096e962f14ae1f4402421ea
baderou <baderou@yahoo.fr>
2023-09-13 13:30:01 +0100

    task 7 done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "6-abs.c"
 
 /**
  * print_last_digit- prints the last digit of a number
@@ -9,5 +10,6 @@
 
 int print_last_digit(int n)
 {
-	return (n % 10);
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
 }
