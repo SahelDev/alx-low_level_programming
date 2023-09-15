64076d3e58df34e9fbb8d48da3d96c4a178b557d
baderou <baderou@yahoo.fr>
2023-09-15 11:07:36 +0100

    task 0 done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -16,16 +16,3 @@ int _isupper(int c)
 			return (1);
 	return (0);
 }
-
-/**int main(void)
-*{
-*	char c;
-*	
-*	c = 'A';
-*	printf("%c: %d\n", c, _isupper(c));
-*	c = 'a';
-*	printf("%c: %d\n", c, _isupper(c));
-*	
-*	return (0);
-*}
-*/
