55d623ce3dd05cc9d8ea1f7ede349a6e8675c91f
baderou <baderou@yahoo.fr>
2023-09-15 11:02:37 +0100

    task 0 done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 
 /**
  * _isupper - checks for uppercase
@@ -16,10 +17,15 @@ int _isupper(int c)
 	return (0);
 }
 
-
-
-
-
-
-
-}
+/**int main(void)
+*{
+*	char c;
+*	
+*	c = 'A';
+*	printf("%c: %d\n", c, _isupper(c));
+*	c = 'a';
+*	printf("%c: %d\n", c, _isupper(c));
+*	
+*	return (0);
+*}
+*/
