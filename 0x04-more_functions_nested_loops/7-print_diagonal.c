c714cb736d577f050b83497c42b0b7ecef9e49b8
baderou <baderou@yahoo.fr>
2023-09-15 17:57:09 +0100

    task 7 done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,22 +1,28 @@
 #include "main.h"
+#include<stdio.h>
 
 /**
- * print_diagonal - print \ the number of times 
- * @n: number of times 
+ * print_diagonal - print \ the number of times
+ * @n: number of times
  * Return: void
  */
 
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	if (n > 0)
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	else
 		_putchar('\n');
