1b7545f417e4260b0e01b4edbadac71c12e4feaa
baderou <baderou@yahoo.fr>
2023-09-15 16:48:25 +0100

    task 6 done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print \ the number of times 
+ * @n: number of times 
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(92);
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
+
+
+
+
+
+}
