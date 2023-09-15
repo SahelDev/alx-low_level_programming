46e9e5f0b370909167b52ab0981d99df4314be17
baderou <baderou@yahoo.fr>
2023-09-15 12:28:13 +0100

    task 5 done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,24 +11,18 @@ void more_numbers(void)
 	int j;
 	int i;
 	int k;
-for (k = 0; k < 10; k++)
-{	
 
-	for (j = 0; j <= 9; j++)
-		putchar('0' + j%10);
-	for (i = 10; i <= 14; i++)
+
+	for (k = 0; k < 10; k++)
+	{
+		for (j = 0; j <= 9; j++)
+			putchar('0' + j % 10);
+		for (i = 10; i <= 14; i++)
 	{
 		putchar('0' + 1);
-		putchar('0' + i%10);
+		putchar('0' + i % 10);
 	}
 
 	putchar('\n');
 }
-}
-
-int main (void)
-{
-	more_numbers();
-
-	return (0);
 }
