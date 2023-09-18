b01acc88e44fa00b65c6477b4780a2aa454685c1
baderou <baderou@yahoo.fr>
2023-09-18 23:57:15 +0100

    task 4 done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ * print_rev - print a string in reverse
+ * @s: a string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	char *p = s;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (s <= p)
+	{
+		putchar(*p);
+		p = p - 1;
+	}
+	putchar('\n');
+}
