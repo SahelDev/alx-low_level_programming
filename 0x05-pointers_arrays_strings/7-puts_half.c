667341f73811a72e6dee1df39d458c3d20523e64
baderou <baderou@yahoo.fr>
2023-09-19 10:41:34 +0100

    task 7 done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - print the second half of the string
+ * @str: parameter 1
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int index;
+	int len = 0;
+	int start;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 != 0)
+	{
+		start = (len + 1) / 2;
+	}
+	else
+	{
+		start = len / 2;
+	}
+
+	for (index = start; index < len; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
