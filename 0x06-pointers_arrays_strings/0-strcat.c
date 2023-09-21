02372ec00e4359f20ad0ed3c44563c49ed699ab7
baderou <baderou@yahoo.fr>
2023-09-21 12:25:14 +0100

    task 0 done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,28 +8,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	while (*dest)
-		dest++;
-	while (*dest++ = *src++)
-		;
-	*dest = '\0';
+	int i;
+	int j;
+
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
 
 	return (dest);
+
 }
-/**
-* int main(void)
-*{
-*	char s1[98] = "Hello ";
-*	char s2[] = "World!\n";
-*	char *ptr;
-*	printf("%s\n", s1);
-*	printf("%s", s2);
-*	ptr = _strcat(s1, s2);
-*	printf("%s", s1);
-*	printf("%s", s2);
-*	printf("%s", ptr);
-*
-*
-*return (0);
-*}
-*/
