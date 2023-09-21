2c92b13ec74da966e32eb7d0b1163f5c3fb220f3
baderou <baderou@yahoo.fr>
2023-09-21 10:08:53 +0100

    task 0 done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -22,6 +22,7 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
 
 
 
