edb54f7820c7e3ddee602dee92b7880a9cc5ea7d
baderou <baderou@yahoo.fr>
2023-09-25 12:39:01 +0100

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+
+
+/**
+ * _memset - fills the first n bytes of memory area pointed to by 
+ * @s: P1
+ * @b: P2
+ * @n: P3
+ *Return: a pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 1;
+
+	while ( i <= n)
+	{
+		*s = b;
+		s++;
+		i++;
+	}
+
+
+
+
+
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Project pointers_array_strings
