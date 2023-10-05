aeea5a4f5859125de051c9f60549290c8e212d1c
baderou <baderou@yahoo.fr>
2023-10-05 10:37:17 +0100

    If you even dream of beating me you

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,12 +14,18 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+
 	tab = (int **)malloc(height * sizeof(int *));
+
 	if (tab == NULL)
+	{
+		free(tab);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 		tab[i] = (int *)malloc(width * sizeof(int));
+
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			tab[i][j] = 0;
