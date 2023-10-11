01249958a645b592d2c67a2bf5559bac19bc6b9c
baderou <baderou@yahoo.fr>
2023-10-11 22:21:28 +0100

    A dog will teach you unconditional

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,18 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		;
-	else
-		if (d->name == NULL || d->owner == NULL)
-			printf("(nil)\n");
-		else
-		{
+	if (d)
+	{
+		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
-			printf("Age: %f\n", d->age);
-			printf("owner: %s\n", d->owner);
-		}
+		else
+			printf("Name:(nil)\n");
+		printf("Age: %.6f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner:(nil)\n");
+	}
+	else
+		return;
 }
