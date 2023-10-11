e967efb5687b6c851f2aebbc4062680ab6efffc1
baderou <baderou@yahoo.fr>
2023-10-11 23:17:08 +0100

    Outside of a dog, a book is a man

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef _DOG_H
-#define _DOG_H_
-
+#define _DOG_H
 /**
  *struct dog - a new structure named dog
  *@name:P1
@@ -15,5 +14,8 @@ struct dog
 };
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+/**
+ * typedef dog_t-defines a new name for type struct dog
+ */
+typedef struct dog dog_t;
 #endif
