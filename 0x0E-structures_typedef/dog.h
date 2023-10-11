5a6ea941b22184933ccaaf6827a9ed4d9c39e219
baderou <baderou@yahoo.fr>
2023-10-11 11:20:11 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+
+/**
+ *struct dog - a new structure named dog
+ *@name:P1
+ *@age:P2
+ *@owner:P3
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
