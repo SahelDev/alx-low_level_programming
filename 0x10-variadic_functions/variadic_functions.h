83e0afd076fdea02df068b06bfeb98a74a9e5944
baderou <baderou@yahoo.fr>
2023-10-14 00:24:01 +0100

    To be is a to be the value of a variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,13 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+typedef struct check
+{
+	char *chk;
+	void (*f)(va_list list);
+} check_t;
+
 
 
 
