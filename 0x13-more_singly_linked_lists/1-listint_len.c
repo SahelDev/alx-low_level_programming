9e622c063d2b894ae6b45cdb3d917661de140ba6
baderou <baderou@yahoo.fr>
2023-10-24 13:35:23 +0100

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,5 @@
 #include"lists.h"
+#include<stdio.h>
 
 /**
  * listint_len-returns the number of elements
@@ -8,10 +9,24 @@
 
 size_t listint_len(const listint_t *h)
 {
+	size_t i = 0;
 
+	if (h == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-
-
-
-
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
+
+
+
+
+
+
