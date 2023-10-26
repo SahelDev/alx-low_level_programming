f8b73e59ad9340f4de0ae5715df88109160ebe67
baderou <baderou@yahoo.fr>
2023-10-26 16:09:10 +0100

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ * print_binary-prints the binary rep
+ * @n:P1
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+
+{
+
+	int c = 0;
+	int bit = sizeof(n) * 8 - 1;
+
+	if (n == 0)
+		_putchar('0');
+
+	while (bit >= 0)
+	{
+		if (n >> bit & 1)
+		{
+			_putchar('1');
+			c++;
+		}
+		else if (c)
+			_putchar('0');
+		bit--;
+	}
+}
