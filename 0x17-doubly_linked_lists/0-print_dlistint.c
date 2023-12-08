f73a80a080ef1141724078d5ab37e1833c4bd6c7
denzel84 <baderou@yahoo.fr>
2023-12-08 15:52:04 +0100

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,18 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
+	const dlistint_t *temp;
 
-	if (h == NULL)
+	temp = h
+
+	if (temp == NULL)
 	{
 		return (-1);
 	}
-	while (h != NULL)
+	while (temp != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", temp->n);
+		temp = temp->next;
 		i += 1;
 	}
 	return (i);
