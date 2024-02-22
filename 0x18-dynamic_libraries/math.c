2791059f0f167bd98ae56e94ef9e1e1ca5a2fc3e
denzel84 <baderou@yahoo.fr>
2024-02-22 14:06:20 +0100

    task 0

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,52 @@
+#include "math.h"
+/**
+ * add - calc add
+ * @a:P1
+ * @b:P2
+ * return:int
+ */
+long int add(int a, int b)
+{
+	return(a + b);
+}
+/**
+ * sub - calc sub
+ * @a:P1
+ * @b:P2
+ * return:int
+ */
+long int sub(int a, int b)
+{
+	return(a - b);
+}
+/**
+ * mul - calc mul
+ * @a:P1
+ * @b:P2
+ * return:int
+ */
+double mul(int a, int b)
+{
+	return(a * b);
+}
+/**
+ * div - calc div
+ * @a:P1
+ * @b:P2
+ * return:int
+ */
+unsigned int div(int a, int b)
+{
+	return(a / b);
+}
+/**
+ * mod - calc mod
+ * @a:P1
+ * @b:P2
+ * return:int
+ */
+unsigned int mod(int a, int b)
+{
+	return(a % b);
+	
+}
---
+++ 0x18-dynamic_libraries/math.h
@@ -0,0 +1,15 @@
+#ifndef MATH_H
+#define MATH_H
+
+long int add(int a, int b);
+long int sub(int a, int b);
+double mul(int a, int b);
+unsigned int div(int a, int b);
+unsigned int mod(int a, int b);
+
+
+
+
+
+
+#endif
